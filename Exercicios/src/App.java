1accb566464d6c562c81c17f413ccd42d01695d6
meduardacarvalho <126161075+meduardacarvalho@users.noreply.github.com>
2023-08-14 15:58:20 -0300

    impar ou par

--- Exercicios/src/App.java
+++ Exercicios/src/App.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class App {
+    public static void ImparPar (int numero) {
+         Scanner sc = new Scanner(System.in);
+System.out.println("digite um numero inteiro: ");
+        numero = sc.nextInt();
+        if (numero %2 == 0){
+            System.out.println("esse numero é par");
+        }
+        else {
+            System.out.println("numero impar");
+        }
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaa");
+       ImparPar(0);
+       
+       
 
     }
 }
